89cf52ddb99a9d4f3fd6037456b52acef81a1f58
Midhula-21 <midhula321@gmail.com>
2025-04-07 04:03:17 -0400

    Fresh start: Re-initialized the project

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,42 +14,43 @@ const Register = () => {
         transform: 'translate(-50%, -50%)', 
         fontFamily: '"Concert One", sans-serif',
         boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-        width : "800px",
+        width : "650px",
+        marginTop : "150px"
           
     }}
     >
 <form>
   <div class="mb-3">
     <label for="exampleInputUsername" class="form-label" style ={{fontSize : "20px",fontWeight : "bold"}}>Full Name</label>
-    <input type="text" class="form-control" id="exampleInputUsername" />
+    <input type="text" class="form-control" id="exampleInputUsername" style={{border:"1px solid black"}}/>
   </div>          
 
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label" style ={{fontSize : "20px",fontWeight : "bold"}}>Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" style={{border:"1px solid black"}} />
   </div>
 
   <div class="mb-3">
     <label for="exampleInputPhone" class="form-label" style ={{fontSize : "20px",fontWeight : "bold"}}>Phone Number</label>
-    <input type="tel" class="form-control" id="exampleInputPhone" />
+    <input type="tel" class="form-control" id="exampleInputPhone" style={{border:"1px solid black"}}/>
   </div>
 
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label" style ={{fontSize : "20px",fontWeight : "bold"}}>Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" />
+    <input type="password" class="form-control" id="exampleInputPassword1" style={{border:"1px solid black"}}/>
   </div>
 
   <div class="mb-3">
     <label for="exampleInputPassword2" class="form-label" style ={{fontSize : "20px",fontWeight : "bold"}}>Confirm Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword2" />
+    <input type="password" class="form-control" id="exampleInputPassword2" style={{border:"1px solid black"}}/>
   </div>
 
   <div className="mb-3">
         <label className="form-label" style ={{fontSize : "20px",fontWeight : "bold"}}>Address</label>
         <input type="text" className="form-control" placeholder="Street Address" required />
-        <input type="text" className="form-control mt-2" placeholder="City" required />
-        <input type="text" className="form-control mt-2" placeholder="State" required />
-        <input type="text" className="form-control mt-2" placeholder="Zip Code" required />
+        <input type="text" className="form-control mt-2" placeholder="City" required style={{border:"1px solid black"}}/>
+        <input type="text" className="form-control mt-2" placeholder="State" required style={{border:"1px solid black"}} />
+        <input type="text" className="form-control mt-2" placeholder="Zip Code" required style={{border:"1px solid black"}}/>
   </div>
   <button type="submit" class="btn btn-primary" style ={{fontSize : "20px",fontWeight : "bold"}}>Register</button>
 </form>
